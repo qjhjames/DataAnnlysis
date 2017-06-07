325f2f36e9e61735d3a9ff0bff7541c549429e37
qiujunhong <qiujunhong1990@126.com>
2017-06-07 22:06:07 +0800

    mac

--- AppAnnlysis.py
+++ AppAnnlysis.py
@@ -1,5 +1,5 @@
 import MyExcelUtil
-
+import matplotlib.pyplot as plt
 #获取数据的所有列并转为数组
 def get_all_col_val_as_array(colnum):
 	fatherArray=[]
@@ -48,16 +48,41 @@ def get_everyday_increase(countArray):
 
 
 def main():
+	group_labels = []
+	x_val = []
 	colnum = MyExcelUtil.get_colnum(0)
 	father = get_all_col_val_as_array(colnum)
 	#print(len(father))
 	count=get_everyday_user(father)
-	print(count)
-	print(get_everyday_increase(count))
-
-
-	print("完毕")
-
+	everyday_all_count=count
+	everday_increase=get_everyday_increase(count)
+	print(everyday_all_count)
+	print(everday_increase)
+	j=0
+	while j<37:
+		temp=j+1
+		group_labels.append("第"+str(temp)+"天")
+		x_val.append(temp)
+		j+=1
+	plt.title("用户使用量")
+	plt.xlabel("日期")
+	plt.ylabel("用户数")
+	#plt.plot(x_val, everyday_all_count, 'r', label="至今用户总量")
+	plt.plot(x_val, everday_increase, 'b', label="当天新增用户量")
+	plt.grid()
+	plt.show()
+	#while j<38:
+	#	temp=j+1
+	#	group_labels.append("第"+temp+"天")
+	#	x_val.append(temp)
+	#	j+=1
+    #plt.title("用户使用量")
+	#plt.xlabel("日期")
+	#plt.ylabel("用户数")
+	#plt.plot(x_val,everyday_all_count,'r',label="至今用户总量")
+	#plt.plot(x_val,everday_increase,'b',label="当天新增用户量"）
+    #plt.grid()
+	#plt.shwo()
 
 if __name__=="__main__":
     main()
