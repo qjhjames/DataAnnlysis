bf5be6acc11d335c09d089545de710e06278656d
qiujunhong <qiujunhong1990@126.com>
2017-10-30 21:20:26 +0800

    mac

--- first.py
+++ first.py
@@ -1,3 +1,23 @@
-from urllib.request import urlopen
-html=urlopen("http://qjhjames.github.io")
-print(html.read())
+import tensorflow as tf
+import numpy as np
+
+x_data = np.float32(np.random.rand(2, 100))
+y_data = np.dot([0.100, 0.200], x_data) + 0.300
+
+b = tf.Variable(tf.zeros([1]))
+W = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
+y = tf.matmul(W, x_data) + b
+
+loss = tf.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in xrange(0, 201):
+    sess.run(train)
+    if step % 20 == 0:
+        print step, sess.run(W), sess.run(b)
